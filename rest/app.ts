c8a4fef886b7222fe2951cb355887b810abb494c
Marco Mendes <corelio@gmail.com>
2023-07-06 19:19:35 -0300

    Melhorias de refatoracao no codigo

--- rest/app.ts
+++ rest/app.ts
@@ -1,3 +1,4 @@
+
 import { Conta } from './cliente/Conta';
 import express, { Request, Response } from 'express';
 
@@ -9,13 +10,64 @@ app.use(express.json());
 
 // Simulação de um banco de dados de contas
 const contas: Conta[] = [
-    new Conta(1, 1000),
+    new Conta(1, 1000, 2000),
   ];
 
+//Funcao para remover uma conta ja existente
+app.delete('/api/contas/:id', async (req: Request, res: Response) => {
+  const idConta = parseInt(req.params.id);
+  const conta = encontrarContaPorId(idConta);
+
+  if (conta) {
+    const indice = contas.indexOf(conta);
+    contas.splice(indice, 1);
+
+    res.status(200).json({ mensagem: 'Conta removida com sucesso' });
+  } else {
+    res.status(404).json({ mensagem: 'Conta não encontrada' });
+  }
+});
+
+// Funcao para atualizar uma conta ja existente
+//Remova limite de credito da chamada da criacao
+app.put('/api/contas/:id', async (req: Request, res: Response) => {
+  const idConta = parseInt(req.params.id);
+  const conta = encontrarContaPorId(idConta);
+
+  if (conta) {
+    const { saldo } = req.body;
+
+    if (!saldo) {
+      return res.status(400).json({ mensagem: 'Dados inválidos' });
+    } else {
+      conta.saldo = saldo;
+    }
+
+    res.status(200).json(conta);
+  } else {
+    res.status(404).json({ mensagem: 'Conta não encontrada' });
+  }
+});  
+ 
+// Funcao para criar uma nova conta
+app.post('/api/contas', async (req: Request, res: Response) => 
+{
+  const { id, saldo, limiteCredito } = req.body;
+
+  if (!id || !saldo || !limiteCredito) {
+    return res.status(400).json({ mensagem: 'Dados inválidos' });
+  }
+
+  const novaConta = new Conta(id, saldo, limiteCredito);
+  contas.push(novaConta);
+
+  res.status(201).json(novaConta);
+});
+  
 // Função para encontrar uma conta pelo ID
-const encontrarContaPorId = (id: number): Conta | undefined => {
+function encontrarContaPorId(id: number): Conta | undefined {
   return contas.find((conta) => conta.id === id);
-};
+}
 
 // Endpoint para consultar o estado da conta
 app.get('/api/contas/:id', async (req: Request, res: Response) => {
@@ -56,3 +108,5 @@ app.listen(PORT, () => {
 });
 
 export default app;
+
+
--- rest/cliente/Conta.ts
+++ rest/cliente/Conta.ts
@@ -1,12 +1,17 @@
-// cliente/Conta.ts
+
+
 export class Conta {
-    constructor(public id: number, public saldo: number) {}
+    constructor(public id: number, public saldo: number, private limiteCredito: number) {}
   
     credito(valor: number): void {
       this.saldo += valor;
     }
   
-    debito(valor: number): void {
-      this.saldo -= valor;
-    }
+    debito(valor: number): boolean {
+      if (valor > this._saldo + this._limite) {
+          return false;
+      }
+      this._saldo -= valor;
+      return true;
+  }
   }
